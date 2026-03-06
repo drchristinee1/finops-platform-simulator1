82bc1e482134a28f2a849ed9a05f7121e2d83ad8
Christine O <83026145+drchristinee1@users.noreply.github.com>
2026-03-06 08:21:34 -0500

    Create demand_forecast_model.py
    Add demand forecast model

---
+++ 01-demand-forecast/demand_forecast_model.py
@@ -0,0 +1,8 @@
+requests = 1200000
+cost_per_request = 0.00042
+
+monthly_cost = requests * cost_per_request
+
+print("Requests:", requests)
+print("Cost per request:", cost_per_request)
+print("Monthly cloud cost:", monthly_cost)
